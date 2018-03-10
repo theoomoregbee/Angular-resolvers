881eaeddf346a0d246069074288046ca70b10567
Theophilus Omorebee <theo4u@ymail.com>
2018-03-11 00:48:06 +0100

    feat: page styling added

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,9 +3,8 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   template: `
-    <p>
-      home works!
-    </p>
+<h6 class="border-bottom border-gray pb-2 mb-0">Home</h6>
+<p style="padding-top: 1rem">Welcome to Angular Resolver</p>
   `,
   styles: []
 })
