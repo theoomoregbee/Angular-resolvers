f2eebf536f60120121affd33b746c626e07f4247
Theophilus Omorebee <theo4u@ymail.com>
2018-03-11 01:01:09 +0100

    feat: added slim loader

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { RecordsComponent } from './records/records.component';
 import { NoResolverComponent } from './no-resolver/no-resolver.component';
-
+import { SlimLoadingBarModule } from 'ng2-slim-loading-bar';
 
 @NgModule({
   declarations: [
@@ -18,7 +18,8 @@ import { NoResolverComponent } from './no-resolver/no-resolver.component';
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(APP_ROUTES)
+    RouterModule.forRoot(APP_ROUTES),
+    SlimLoadingBarModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
