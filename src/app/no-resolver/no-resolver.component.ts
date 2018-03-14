2c267c7e5b3101d551a8f632075451d6731117e1
Theophilus Omorebee <theo4u@ymail.com>
2018-03-14 01:10:25 +0100

    feat: no resolver flow added

--- src/app/no-resolver/no-resolver.component.ts
+++ src/app/no-resolver/no-resolver.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { MockHttpService } from '../services/mock-http.service';
+import { IData } from '../interfaces/idata';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/forkJoin';
+import 'rxjs/add/operator/map';
+
+import { NgAlertService, MessageType  } from '@theo4u/ng-alert';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-no-resolver',
@@ -6,10 +14,32 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class NoResolverComponent implements OnInit {
+  data: IData[] = [];
+  dynamicTitle: string;
+  loader = false;
 
-  constructor() { }
+  constructor(private _activatedRoute: ActivatedRoute, private _mockService: MockHttpService, private _ngAlert: NgAlertService) {  }
 
   ngOnInit() {
+    this.loader = true;
+    // subscripe to the param here, this is just for quick demo test
+     const withError = this._activatedRoute.snapshot.params['with-error'] === 'true';
+Observable.forkJoin ([
+      this._mockService.dynamicTitle('resolver'),
+      this._mockService.request(withError)
+    ])
+    .subscribe(results => {
+        this.dynamicTitle = results[0];
+        this.data = results[1];
+      this.loader = false;
+    }, error => {
+      this._ngAlert.push({
+        message: error.message,
+        type: MessageType.error
+      });
+      this.loader = false;
+    });
+
   }
 
 }
